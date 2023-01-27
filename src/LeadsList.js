f4e1c8a8b2b47436a6cff9beea2e5cd296ba8681
Diana Bautista Bautista <ada_wit60@hackademy.mx>
2023-01-27 12:21:52 -0500

    feat: adding lead table for webScraper

---
+++ src/LeadsList.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { leadsData } from "./leads";
+import BootstrapTable from "react-bootstrap-table-next";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.css";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
+function LeadsList() {
+  const [leadList, setLeadList] = useState(leadsData);
+
+  const columns = [
+    { dataField: "name", text: "Razón Social" },
+    // { dataField: "logo", text: "Logo" },
+    { dataField: "data", text: "Información de contacto" },
+    { dataField: "tiers", text: "Tiers", sort: true },
+    { dataField: "teus", text: "Teus", sort: true },
+  ];
+
+  const pagination = paginationFactory({
+    page: 1,
+    sizePerPage: 5,
+    lastPageTest: " >>>",
+    firstPagetext: ">",
+    prePageText: "<",
+    showTotal: true,
+    alwaysShowAllBtns: true,
+    onPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    },
+    onSizePerPageChange: function (page, sizePerPage) {
+      console.log("page", page);
+      console.log("sizePerPage", sizePerPage);
+    },
+  });
+
+  return (
+    <div>
+      <BootstrapTable
+        bootstrap4
+        keyField="name"
+        columns={columns}
+        data={leadList}
+        pagination={pagination}
+      />
+    </div>
+  );
+}
+
+export default LeadsList;
---
+++ src/leads.js
@@ -0,0 +1,77 @@
+export const leadsData = [
+  {
+    name: "PLASTICOS ROCA S.A.C.",
+    logo: "https://expoplastperu.com/guia/images/anuncios-empresas/5eeff36021799-logo.png",
+    data: [
+      "+51 1 7479497",
+      "+51 999335633",
+      "ventas@plasticosroca.com.pe",
+      "http://www.plasticosroca.com.pe",
+    ],
+    category: "envases-plasticos",
+    tiers: 1,
+    teus: 3,
+  },
+  {
+    name: "TECNIPACK S.A.C.",
+    logo: "https://expoplastperu.com/guia/images/anuncios-empresas/5f2378fec591a-logo.png",
+    data: [
+      "+51 1 3486647  +51 1 3483749",
+      "ventas@tecnipacksac.com",
+      "http://www.tecnipacksac.com",
+    ],
+    category: "envases-plasticos",
+    tiers: 2,
+    teus: 9,
+  },
+  {
+    name: "EUROPLAST S.A.C.",
+    logo: "https://expoplastperu.com/guia/images/anuncios-empresas/5ef0257c57fed-logo.png",
+    data: [
+      "+51 1 3480886",
+      "WhatsApp +51 998369875",
+      "info@europlast.pe",
+      "http://www.europlast.pe",
+    ],
+    category: "envases-plasticos",
+    tiers: 1,
+    teus: 4,
+  },
+  {
+    name: "CORSUN S.A.C.",
+    logo: "https://expoplastperu.com/guia/images/anuncios-empresas/5f1a725f71a8f-logo.png",
+    data: [
+      "+51 1 4404191",
+      "comercial@corsun.com.pe",
+      "http://www.corsun.com.pe",
+    ],
+    category: "envases-plasticos",
+    tiers: 2,
+    teus: 3,
+  },
+  {
+    name: "MELAFORM S.A.C.",
+    logo: "https://expoplastperu.com/guia/images/anuncios-empresas/5ef03adde3060-logo.png",
+    data: [
+      "+51 1 4590026",
+      "ventasnacionales@melaform.com.pe",
+      "http://www.melaform.com.pe",
+    ],
+    category: "envases-plasticos",
+    tiers: 2,
+    teus: 2,
+  },
+  {
+    name: "VIGOFLEX PACKAGING ",
+    logo: "https://expoplastperu.com/guia/images/anuncios-empresas/62aa08310a10f-logo.png",
+    data: [
+      "(Perú) +51 1 480 0227 / (Chile) +56 2 2904 9925",
+      "+51 912 267 183",
+      "info@vigoflex.com",
+      "http://www.vigoflex.com",
+    ],
+    category: "envases-plasticos",
+    tiers: 0,
+    teus: 5,
+  },
+];
